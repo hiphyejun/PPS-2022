459e9559d64578425f74332b26b2db6d789cd170
Hyejun Kim <107266952+hiphyejun@users.noreply.github.com>
2022-07-16 20:01:40 +0900

    Create A056_김혜준_20220716.java

---
+++ Week2/A056_김혜준_20220716.java
@@ -0,0 +1,16 @@
+public int[] nextGreaterElementBrute(int[] nums1, int[] nums2) {
+	int[] ans = new int[nums1.length];
+
+	for(int i = 0; i < nums1.length; i++) {
+		int greaterIdx = -1, j = nums2.length - 1;
+		while(j >= 0 && nums2[j] != nums1[i]) {
+			if(nums2[j] > nums1[i]){
+				greaterIdx = nums2[j];
+			}
+			j--;
+		}
+		ans[i] = greaterIdx;
+	}
+
+	return ans;
+}
